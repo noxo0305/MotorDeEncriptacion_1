34ba28c4091db0e054b463f9969e0b37322f2cda
Jose-Dv <117058237+Jose-Dv@users.noreply.github.com>
2024-09-06 06:48:17 -0500

    Update consola.py

--- consola.py
+++ consola.py
@@ -1,33 +1,36 @@
 from funcionalidad import *
-def recibir_matriz():
-    """
-    Recibe una matriz 2x2 del usuario.
-    """
-    clave = []
-    for _ in range(2):
-        fila = []
-        for _ in range(2):
-            valor = int(input("Ingrese un valor de la clave (total de 4 valores): "))
-            fila.append(valor)
-        clave.append(fila)
-    print("La clave ingresada es:", clave)
-    return clave
-def main():
-    clave = recibir_matriz()
-    while not  tamaño_matriz(clave):
-        clave = recibir_matriz()
-    encriptador = Encriptador(clave)
-    opcion = int(input("Ingrese 1 si desea encriptar y 2 si desea desencriptar: "))
-    while opcion not in [1, 2]:
-        print("Ingrese una opción válida.")
-        opcion = int(input("Ingrese 1 si desea encriptar y 2 si desea desencriptar: "))
 
-    if opcion == 1:
-        mensaje = input("Ingrese el mensaje a encriptar: ")
-        mensaje_encriptado = encriptador.encriptar(mensaje)
-        print("Mensaje encriptado:", mensaje_encriptado)
-    elif opcion == 2:
-        mensaje_encriptado = np.array(eval(input("Ingrese el mensaje encriptado (como lista de listas): ")))
-        mensaje_desencriptado = encriptador.desencriptar(mensaje_encriptado)
-        print("Mensaje desencriptado:", mensaje_desencriptado)
+def receive_matrix():
+    """
+    Receives a 2x2 matrix from the user.
+    """
+    key = []
+    for _ in range(2):
+        row = []
+        for _ in range(2):
+            value = int(input("Ingrese un valor de la clave (total de 4 valores): "))
+            row.append(value)
+        key.append(row)
+    print("The entered key is:", key)
+    return key
+
+def main():
+    key = receive_matrix()
+    while not matrix_size(key):
+        key = receive_matrix()
+    encryptor = Encryptor(key)
+    option = int(input("Ingrese 1 si desea encriptar y 2 si desea desencriptar: "))
+    while option not in [1, 2]:
+        print("Enter a valid option.")
+        option = int(input("Ingrese 1 si desea encriptar y 2 si desea desencriptar: "))
+
+    if option == 1:
+        message = input("Ingrese el mensaje a encriptar: ")
+        encrypted_message = encryptor.encrypt(message)
+        print("Encrypted message:", encrypted_message)
+    elif option == 2:
+        encrypted_message = np.array(eval(input("Ingrese el mensaje encriptado (como lista de listas): ")))
+        decrypted_message = encryptor.decrypt(encrypted_message)
+        print("Decrypted message:", decrypted_message)
+
 main()
