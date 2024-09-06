c0afc25213dc2fd82494943d3737616b4f9a099d
Jose-Dv <117058237+Jose-Dv@users.noreply.github.com>
2024-09-05 22:26:02 -0500

    Add files via upload

--- consola.py
+++ consola.py
@@ -1,21 +1,21 @@
 from funcionalidad import *
-
-clave = recibir_matriz()
-while not tamaño_matriz(clave):
-    print("la matriz ingresada no cumple los requerimentos")
+if __name__ == "__main__":
     clave = recibir_matriz()
-encriptador = Encriptador(clave)
-print("ingrese 1 si desea encriptar y 2 si deseea desencriptar")
-control = int(input("ingrese una opcion:"))
-while control != 1 and control != 2:
-    print("ingresa una opcion valida")
-    print("ingrese 1 si desea encriptar y 2 si deseea desencriptar")
-    control = int(input("ingrese una opcion:"))
-if control == 1:
-    mensaje = str(input("ingrese el mesnaje a incriptar:"))
-    mensaje_encriptado = encriptador.encriptar(mensaje)
-    print("Mensaje encriptado:", mensaje_encriptado)
-if control == 2:
-    mensaje = str(input("ingrese el mesnaje a incriptar:"))
-    mensaje_desencriptado = encriptador.desencriptar(mensaje_encriptado)
-    print("Mensaje desencriptado:", mensaje_desencriptado)
+    while not tamaño_matriz(clave):
+        print("La matriz ingresada no cumple los requerimientos.")
+        clave = recibir_matriz()
+
+    encriptador = Encriptador(clave)
+    opcion = int(input("Ingrese 1 si desea encriptar y 2 si desea desencriptar: "))
+    while opcion not in [1, 2]:
+        print("Ingrese una opción válida.")
+        opcion = int(input("Ingrese 1 si desea encriptar y 2 si desea desencriptar: "))
+
+    if opcion == 1:
+        mensaje = input("Ingrese el mensaje a encriptar: ")
+        mensaje_encriptado = encriptador.encriptar(mensaje)
+        print("Mensaje encriptado:", mensaje_encriptado)
+    elif opcion == 2:
+        mensaje_encriptado = np.array(eval(input("Ingrese el mensaje encriptado (como lista de listas): ")))
+        mensaje_desencriptado = encriptador.desencriptar(mensaje_encriptado)
+        print("Mensaje desencriptado:", mensaje_desencriptado)
--- funcionalidad.py
+++ funcionalidad.py
@@ -1,52 +1,62 @@
 import numpy as np
 
-
 class Encriptador:
     def __init__(self, clave):
+        """
+        Inicializa el encriptador con una clave y calcula su inversa.
+        """
         self.clave = np.array(clave)
         self.inversa_clave = np.linalg.inv(self.clave)
 
     def encriptar(self, mensaje):
+        """
+        Encripta un mensaje utilizando la clave.
+        """
         mensaje_vector = self._convertir_a_vector(mensaje)
         mensaje_encriptado = np.dot(self.clave, mensaje_vector)
         return mensaje_encriptado
 
     def desencriptar(self, mensaje_encriptado):
+        """
+        Desencripta un mensaje encriptado utilizando la inversa de la clave.
+        """
         mensaje_vector = np.dot(self.inversa_clave, mensaje_encriptado)
         mensaje_desencriptado = self._convertir_a_texto(mensaje_vector)
         return mensaje_desencriptado
 
     def _convertir_a_vector(self, mensaje):
+        """
+        Convierte un mensaje de texto a un vector numérico.
+        """
         mensaje_vector = [ord(char) for char in mensaje]
         while len(mensaje_vector) % self.clave.shape[0] != 0:
             mensaje_vector.append(0)
         return np.array(mensaje_vector).reshape(-1, self.clave.shape[0]).T
 
     def _convertir_a_texto(self, mensaje_vector):
+        """
+        Convierte un vector numérico a un mensaje de texto.
+        """
         mensaje_vector = mensaje_vector.T.flatten()
         mensaje = ''.join(chr(int(round(num))) for num in mensaje_vector if num != 0)
         return mensaje
 
-
 def tamaño_matriz(matriz):
-    if len(matriz) == 2:
-        if len(matriz[0]) == 2:
-            if len(matriz[1]) == 2:
-                return True
-    else:
-        return False
-
+    """
+    Verifica si la matriz es de tamaño 2x2.
+    """
+    return len(matriz) == 2 and all(len(fila) == 2 for fila in matriz)
 
 def recibir_matriz():
+    """
+    Recibe una matriz 2x2 del usuario.
+    """
     clave = []
-    index = 0
-    while True:
-        index += 1
-        i = int(input("ingrese un valor de la clave para un total de 4:"))
-        j = int(input("ingrese un valor de la clave para un total de 4:"))
-        clave.append([i, j])
-        if index == 2:
-            break
-    print("la clave ingresada es:", clave)
+    for _ in range(2):
+        fila = []
+        for _ in range(2):
+            valor = int(input("Ingrese un valor de la clave (total de 4 valores): "))
+            fila.append(valor)
+        clave.append(fila)
+    print("La clave ingresada es:", clave)
     return clave
-
